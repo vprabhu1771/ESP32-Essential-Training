448db25270364a445bcb1f5f662147f0c4ee8544
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2025-05-09 12:21:58 +0530

    Update BlinkLED.py

--- MicroPython/BlinkLED.py
+++ MicroPython/BlinkLED.py
@@ -4,18 +4,21 @@ import time
 
 led  = machine.Pin(2, machine.Pin.OUT)
 
+
 while True:
     led.on()
     time.sleep(0.5)
     led.off()
     time.sleep(0.5)
 
+# OR
 while True:
     led.value(1)
     time.sleep(0.5)
     led.value(0)
     time.sleep(0.5)
 
+# OR
 while True:
     led.value(True)
     time.sleep(0.5)
