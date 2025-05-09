f7b851108a46f8355aa88f0fba852ce97254dbb2
Prabhu <vprabhu1771@gmail.com>
2025-05-09 11:59:38 +0530

    UPDATE

---
+++ MicroPython/BlinkLED.py
@@ -0,0 +1,23 @@
+import machine
+
+import time
+
+led  = machine.Pin(2, machine.Pin.OUT)
+
+while True:
+    led.on()
+    time.sleep(0.5)
+    led.off()
+    time.sleep(0.5)
+
+while True:
+    led.value(1)
+    time.sleep(0.5)
+    led.value(0)
+    time.sleep(0.5)
+
+while True:
+    led.value(True)
+    time.sleep(0.5)
+    led.value(False)
+    time.sleep(0.5)
